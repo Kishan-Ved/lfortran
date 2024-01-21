57d09043815361ca1738b858dda825f6ee335c9e
Kishan-Ved <kishan.ved@iitgn.ac.in>
2024-01-22 01:53:41 +0530

    test3

--- integration_tests/intrinsics_122.f90
+++ integration_tests/intrinsics_122.f90
@@ -4,6 +4,7 @@ program intrinsic_122
     real, allocatable :: e(:,:)
   
     a = reshape([1, 2, 3, 4, 5, 6], [2, 3])
+    
     b = reshape([10, 20, 30, 40, 50, 60], [3, 2])
   
     print*, rank(1)
